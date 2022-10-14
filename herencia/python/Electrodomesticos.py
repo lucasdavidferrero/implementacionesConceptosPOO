37ce6f28b77799a77a51e8bf185f0c787ab1db4e
Lucas Ferrero <lucasdavidferrero@gmail.com>
2022-10-14 15:01:13 -0300

    Agrego modelo de herencia. Implemento Herencia en Python.

---
+++ herencia/python/Electrodomesticos.py
@@ -0,0 +1,27 @@
+class Electrodomestico:
+    """Clase abstracta"""
+    marca = ''
+    modelo = ''
+    nroSerie = 0
+
+    def __init__(self, marca, modelo, nroSerie):
+        self.marca = marca
+        self.modelo = modelo
+        self.nroSerie = nroSerie
+
+
+class Lavadora(Electrodomestico):
+    capacidadMaximaKg = 0
+    cargaActualKg = 0
+
+    # Se invoca el constructor de la clase Base
+    def __init__(self, marca, modelo, nroSerie, capacidad):
+        Electrodomestico.__init__(self, marca, modelo, nroSerie)
+        self.capacidadMaximaKg = capacidad
+
+    def agregarRopa(self, pesoPrendaKg):
+        if (pesoPrendaKg < self.capacidadMaximaKg):
+            self.cargaActualKg += pesoPrendaKg
+
+    def sacarRopa(self):
+        self.cargaActualKg = 0
---
+++ herencia/python/herencia.py
@@ -0,0 +1,10 @@
+import Electrodomesticos
+
+drean = Electrodomesticos.Lavadora('Drean', 'Concept 5.05', 16658, 5)
+
+drean.agregarRopa(1)
+drean.agregarRopa(0.55)
+drean.agregarRopa(0.22)
+
+print('Marca: ', drean.marca)
+print('carga actual: ', drean.cargaActualKg)
